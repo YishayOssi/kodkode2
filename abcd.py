ab09ddaa1276b6a4564a27d744e3d71a29c211b6
yishay ossi <yishayossi10@gmail.com>
2025-06-19 16:17:04 +0300

    yishay king

---
+++ abcd.py
@@ -0,0 +1,47 @@
+#1
+# def Hello_world():
+#     print("Hello World")
+#
+# Hello_world()
+
+#2
+# def multiplication(x, y):
+#     print("The numbers entered are:", x,",",y)
+#     print("The product of the two numbers:",x * y)
+# multiplication(2, 4)
+
+#3
+# def listi_number(x, y):
+#     sum = x+y
+#     multiply = x*y
+#     return [sum, multiply]
+#
+# z = listi_number(2, 4)
+# print(z)
+
+#4
+# z = int(input("enter a number:"))
+#
+# def True_or_False(z):
+#     if z % 2 == 0:
+#         return "true"
+#     else:
+#         return "false"
+#
+# a = True_or_False(z)
+# print(a)
+
+#5
+def str(x, y, z):
+    return x+y+z
+#
+# a = str("aaa", "bbb", "ccc")
+# print(a)
+
+#6
+# def str_reverse(x):
+#     return x[::-1]
+# y = (input("Give me a word:"))
+# z = str_reverse(y)
+# print(z)
+
